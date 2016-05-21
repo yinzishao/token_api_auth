ff163ade29a2b42eef5cbeb83b0493076263776c
yinzishao <370953598@qq.com>
2016-05-21 21:19:28 +0800

    5/21 token isert in redis

--- auth_token/views.py
+++ auth_token/views.py
@@ -8,8 +8,18 @@ from django.contrib.auth.models import User
 from django.template.context_processors import csrf
 from tokenapi.views import token_new
 from django.db import IntegrityError
-
+from django.core.signing import Signer,TimestampSigner
 from tokenapi.decorators import token_required
+from datetime import timedelta
+from redisutil import get_userpk,delete_token,insert_token
+
+
+token_cahce_salt = "make_token_in_cache"
+time_signer = TimestampSigner(salt=token_cahce_salt)
+user_salt = "user_salt"
+user_signer = Signer(salt=user_salt)
+
+
 # @token_required
 def loginview(request):
     # c = {"yin":"yin"}
@@ -57,35 +67,135 @@ def signup(request):
         # data['password']=password
         # return HttpResponse(json.dumps(data),content_type="application/json")
 
-    return HttpResponse("No post")
+    return HttpResponse("signup fail")
 
-def test(requset):
-    username="yzs"
-    password="pwd"
-    # User.objects.
-    if username:
+#根据id和timestamp生成token
+def make_token_in_cache(user):
+
+    userpk = user.pk
+    user_str = "userpk"+"-"+str(userpk)
+    token = time_signer.sign(user_str)
+    return token
+    # password =user.password
+    # id_pwd=str(userid)+"-"+password
+    # value = singer.sign(id_pwd)
+        # return HttpResponse(id_pwd)
+        # value = singer.sign('token')
+
+#根据token和userpk判断是否出现和篡改
+def check_token_in_cache(data):
+    token = data['token']
+    userpk= data['userpk']
+    #过期时间，默认10天
+    max_age= 60*60*24*10
+    # time_signer.unsign(token)
+    userpk_origin = get_userpk(token)
+    if userpk_origin:
+        user_str = "userpk"+"-"+str(userpk_origin)
+        value = user_str+":"+token
         try:
-            user = User.objects.get_by_natural_key(username)
-        except User.DoesNotExist:
-            user = User.objects.create_user(username,"yz",password=password)
-            if user.is_active:
-                return HttpResponse("success")
-            else:
-
-                return HttpResponse("fail")
-
+            time_signer.unsign(value,max_age=max_age)
+        except Exception,e:
+            print e.message
+            delete_token(token)
+            return HttpResponse("expired please login in again")
         else:
-            return HttpResponse("User already exists")
-#
-    # try:
-    #     pass
-    # except IntegrityError:
-    #     return HttpResponse("Fail")
+            userpk_str = str(userpk_origin)+":"+str(userpk)
+            # print userpk_str,type(userpk_str),id(userpk_str)
+            userpk_sige_str=user_signer.sign(userpk_origin)
+            # print userpk_sige_str,type(userpk_sige_str),id(userpk_sige_str)
+            if userpk_str ==userpk_sige_str:
+                #更新token时间?
+
+                return HttpResponse("succeed")
+            else:
+                return HttpResponse("Login in Fail PK ")
+
+    else:
+        return HttpResponse("Login in Fail")
+
+    #
+    # user_str = "userpk"+"-"+str(userpk)
+    # value = user_str+":"+token
+    # try :
+    #     time_signer.unsign(value)
+    # except:
+    #     return HttpResponse("Token is invalid")
     # else:
-    #     if user:
-    #         return HttpResponse("succeed")
+    #     return HttpResponse("succeed")
+
+
+
+#用户密码登录返回token
+def login_from_pwd(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+    # username="yzs"
+    # password="pwd"
+        if username and password:
+            user = authenticate(username=username,password=password)
+
+            if user and user.is_active:
+                login(request,user)
+                token = make_token_in_cache(user).split(":",1)[1]
+
+                #将{token:id}放在redis内
+                redis_data = {
+                    'token':token,
+                    'userpk':user.pk,
+                }
+                insert_token(redis_data)
+                #将pk加密
+                userpk = user_signer.sign(user.pk).split(":",1)[1]
+
+                data = {
+                    'token':token,
+                    'userpk':userpk,
+                }
+                return HttpResponse(json.dumps(data),content_type="application/json")
+            else:
+                return HttpResponse("Fail")
+def test(request):
+    data={
+        "token": "1b46US:_uw-1cM6p3M8H10r7SF3DR6EQCk",
+        "userpk": "FgsrjCxMETo6hgMNoeR8Tufa1-o",
+    }
+    return check_token_in_cache(data)
+
+    #
+    # """
+    # 测试密码登陆
+    # """
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    # username="yzs"
+    # password="pwd"
+    # if username and password:
+    #     user = authenticate(username=username,password=password)
+    #
+    #     if user and user.is_active:
+    #         login(request,user)
+    #         token = make_token_in_cache(user).split(":",1)[1]
+    #
+    #         #将{token:id}放在redis内
+    #         redis_data = {
+    #             'token':token,
+    #             'userpk':user.pk,
+    #         }
+    #         insert_token(redis_data)
+    #         #将pk加密
+    #         userpk = user_signer.sign(user.pk).split(":",1)[1]
+    #
+    #         data = {
+    #             'token':token,
+    #             'userpk':userpk,
+    #         }
+    #         return HttpResponse(json.dumps(data),content_type="application/json")
     #     else:
-    #         return HttpResponse("fail")
+    #         return HttpResponse("Fail")
+
 
 def createUser(**kwargs):
     username = kwargs['username']
